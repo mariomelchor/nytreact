c45913226ba3e53adbbb5137cea212f4f2da10cb
mariomelchor <mario@mariomelchor.com>
2017-10-28 23:26:26 -0700

    Added Articles model, Added mongoose as a dependecy

---
+++ src/models/Article.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+var Schema = mongoose.Schema;
+
+// Create a new ArticleSchema object
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+});
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+export default Article;
