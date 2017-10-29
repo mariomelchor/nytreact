c6eb80e686dbc7fb4cb03749cab1af743d1c20a5
mariomelchor <mario@mariomelchor.com>
2017-10-28 21:44:27 -0700

    Added materialize css, js, Added masonry css, js, Created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => (
+  <div className="navbar-fixed">
+    <nav className="site-navbar blue darken-4">
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo"><i className="material-icons left">library_books</i> NYT Scraper React App</a>
+        <ul className="right">
+          <li><a href="/"><i className="material-icons left tiny">home</i> Home</a></li>
+          <li><a href="/saved"><i className="material-icons left tiny">save</i> Saved Articles</a></li>
+          <li><a href="/scrape" id="btn-scrape"className="btn waves-effect waves-light orange"><i className="material-icons left tiny">clear_all</i> Scrape Articles</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
+export default Navbar;
